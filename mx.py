a24fafbb9cf4ca00ed622b1b3ddd00f62286d007
plhannakabakj <kagdhzisbabahakajjzk@outlook.com>
2025-02-18 16:49:15 +0530

    Update mx.py

--- mx.py
+++ mx.py
@@ -3,8 +3,8 @@ import subprocess
 import datetime
 import os
 
-from keep_alive import keep_alive
-keep_alive()
+
+
 # Insert your Telegram bot token here
 bot = telebot.TeleBot('7526972070:AAHvzU-FMBfzxFxk26HzFAMqixadTZo_Bjo')
 
